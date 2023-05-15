38f17775707cdf2d081f9d2ed7d1892ed6d33198
JhefRhey <120020714+JhefRhey@users.noreply.github.com>
2023-05-15 11:56:47 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,32 +13,22 @@ function Navbar() {
     setOpenLinks(!openLinks);
   };
   return (
-  // <div className="navbar">
-    //  <div className="leftSide" id={openLinks ? "open" : "close"}>
-      //  <img src={Logo} alt="loading" >
-        //<div className="hiddenLinks">
-          //<Link to="/"> Home </Link>
-          //<Link to="/menu"> Menu </Link>
-          //<Link to="/about"> About </Link>
-          //<Link to="/contact"> Contact </Link>
-        //</div>
-      //</div> 
-<div className="navbar">
-    
+   <div className="navbar">
+       <div className="leftSide" id={openLinks ? "open" : "close"}>
+       <img src={Logo} alt="loading" />
        <div className="hiddenLinks">
-         <Link to="/"> Home </Link>
+          <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
-      </div>
 
-      <div className="rightSide">
-        <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/contact"> Contact </Link>
-        <button onClick={toggleNavbar}>
+        <div className="rightSide">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+          <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
       </div>
